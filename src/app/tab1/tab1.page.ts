a8fb3afcdac41bd6a2bca0b2e3753b433aae0ce3
richard <richard.millett+github@googlemail.com>
2021-04-27 20:30:11 +0100

    Adding some buttons pop ups

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActionSheetController} from '@ionic/angular';
+import {AlertController} from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,7 +9,9 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  // constructor() {}
+  constructor(public actionSheetController: ActionSheetController, private alertController: AlertController) {
+  }
 
   disableButton(element) {
     element.setAttribute('disabled', 'true');
@@ -20,5 +24,61 @@ export class Tab1Page {
     } else {
       element.setAttribute('color', 'primary');
     }
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        role: 'destructive',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const {role} = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      cssClass: 'basic-alert',
+      header: 'Alert Header',
+      subHeader: 'Alert Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 }
